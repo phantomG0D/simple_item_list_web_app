43e1e3e4ce5be448d327653d5b007b0573032919
phantom0 <official.nobinsutradhar@gmail.com>
2025-09-10 13:08:43 +0530

    just Changed the items

--- template_demo/items/views.py
+++ template_demo/items/views.py
@@ -4,7 +4,13 @@ from django.shortcuts import render
 def item_list(request):
     context = {
         'title': 'My Brutal Item List',
-        'items': ['Apple', 'Banana', 'Cherry', 'Durian'],
+        'items':
+        ["Exploit Weakness",     # Find every vulnerability (in systems, people, yourself) and use it.
+    "Dominate Time",        # Own the clock. Schedule, execute, repeat without mercy.
+    "Hoard Knowledge",      # Accumulate skills, intel, and secrets no one else bothers to get.
+    "Crush Resistance" 
+    ],
+
         'user_authenticated': True,
         'user_name': 'phantom0'
     }
